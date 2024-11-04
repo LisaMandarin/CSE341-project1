0f259bf4a2108cae36d54d9a48628630f437011d
LisaMandarin <chu23001@byui.edu>
2024-11-05 03:12:38 +0800

    add root route

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 const router = require("express").Router()
 
+router.get("/", (req, res) => {
+    res.status(200).send("Hello, World")
+})
 router.use("/users", require("./user.js"))
 
 module.exports = router
